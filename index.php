4da25dfc9814e4721fbb241e8a818fa7e30a6a83
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-06-07 20:23:05 +0200

    parking filter

--- index.php
+++ index.php
@@ -40,6 +40,36 @@
 
     ];
 
+    $park = isset($_GET['park']) ? $_GET['park'] : '';
+    $voto = isset($_GET['voto']) ? $_GET['voto'] : '';
+
+
+    if($park === 'true'){
+        $filterhotels = array_filter($hotels, function ($hotel){
+           return $hotel['parking'] === true;
+
+        });
+    }else if ($park === 'false'){
+        $filterhotels = array_filter($hotels, function ($hotel){
+            return $hotel['parking'] === false;
+         });
+    }else{
+        $filterhotels = $hotels;
+    };
+
+    
+    // if($voto > 3){
+    //     $filterhotels = array_filter($hotels, function ($hotel){
+    //         return $hotel['vote'] > 3;
+    // });
+    // }else if($voto < 3){
+    //     $filterhotels = array_filter($hotels, function ($hotel){
+    //         return $hotel['vote'] < 3;
+    // });
+    // }else{
+    //     $filterhotels = $hotels;
+    // }
+
 ?>
 
 
@@ -49,12 +79,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
     <title>Document</title>
 </head>
 <body>
+
+<div class="container py-3">
+    <form action="" method="get" class="d-flex">
+    <select class="form-select" aria-label="Default select example" id="select" name="park">
+        <option selected>Disponibilità parcheggio...</option>
+        <option value="true">Con parcheggio</option>
+        <option value="false">Senza parcheggio</option>
+    </select>
+    <select class="form-select" aria-label="Default select example" id="select" name="voto">
+        <option selected>Seleziona voto...</option>
+        <option value="1">1 Stella</option>
+        <option value="2">2 Stelle</option>
+        <option value="3">3 Stelle</option>
+        <option value="4">4 Stelle</option>
+        <option value="5">5 Stelle</option>
+    </select>
+    <button type="submit">Cerca</button>
+    </form>
+</div>
+
+
+
+
 
 
 <div class="container py-5 my-5">
@@ -71,17 +123,17 @@
   <tbody>
     <tr>
         <?php
-                foreach($hotels as $key => $hotel){ ?>
+                foreach($filterhotels as $key => $hotel){ ?>
             <th scope="row"><?php echo $hotel['name'] ?></th>
             <td><?php echo $hotel['description'] ?></td>
-            <td><?php if($hotel['parking'] == true){?>
-                            SI <?php }
-                            else { ?>
+            <td><?php if($hotels[$key]['parking'] == true){?>
+                            SI <?php 
+                            } else { ?>
                             NO <?php  
                             }
                             ?></td>
             <td><?php echo $hotel['vote'] ?></td>
-            <td><?php echo $hotel['distance_to_center'] ?></td>
+            <td><?php echo $hotel['distance_to_center'] . ' ' . 'Km' ?></td>
     </tr>
         <?php 
         }?>
