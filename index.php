36b940fc09e05fce5ca30132c7c3ee9d217496d0
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-06-07 19:00:32 +0200

    informations displayed

--- index.php
+++ index.php
@@ -49,11 +49,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        
-    </ul>
+
+
+<div class="container py-5 my-5">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome Hotel:</th>
+      <th scope="col">Descrizione:</th>
+      <th scope="col">Parcheggio:</th>
+      <th scope="col">Voto:</th>
+      <th scope="col">Distanza dal centro:</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <?php
+                foreach($hotels as $key => $hotel){ ?>
+            <th scope="row"><?php echo $hotel['name'] ?></th>
+            <td><?php echo $hotel['description'] ?></td>
+            <td><?php if($hotel['parking'] == true){?>
+                            SI <?php }
+                            else { ?>
+                            NO <?php  
+                            }
+                            ?></td>
+            <td><?php echo $hotel['vote'] ?></td>
+            <td><?php echo $hotel['distance_to_center'] ?></td>
+    </tr>
+        <?php 
+        }?>
+  </tbody>
+</table>
+</div> 
 </body>
 </html>
+
+
